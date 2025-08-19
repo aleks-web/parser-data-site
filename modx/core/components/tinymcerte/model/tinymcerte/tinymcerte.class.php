01eda321cff2094d31f4fb00e9a7d205dd289cec
Alex <dok.go@yandex.ru>
2025-08-19 10:20:41 +0500

    The commit is made from the makefile

---
+++ modx/core/components/tinymcerte/model/tinymcerte/tinymcerte.class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * TinyMCE Rich Text Editor
+ *
+ * @package tinymcerte
+ * @subpackage classfile
+ */
+
+require_once dirname(__DIR__, 2) . '/vendor/autoload.php';
+
+/**
+ * Class TinyMCERTE
+ */
+class TinyMCERTE extends \TinyMCERTE\TinyMCERTE
+{
+}
---
+++ modx/core/components/tinymcerte/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'modxcms/tinymcerte',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9491ae2508d22718c4ffa22f44543e4701fe36ba',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'modxcms/tinymcerte' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9491ae2508d22718c4ffa22f44543e4701fe36ba',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
